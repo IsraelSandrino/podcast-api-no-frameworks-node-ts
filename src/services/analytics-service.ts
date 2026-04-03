673ef3ba6d92650a301833099c71027169cec4b7
IsraelSandrino <isandrino99@gmail.com>
2026-04-03 00:35:00 -0300

    feat: add register play, episode and podcast analytics services

---
+++ src/services/analytics-service.ts
@@ -0,0 +1,48 @@
+import { AnalyticsTransferModel } from "../models/analytics-transfer-model";
+import {
+  repositoryEpisodeAnalytics,
+  repositoryPodcastAnalytics,
+  repositoryRegisterPlay,
+} from "../repositories/analytics-repository";
+import { StatusCode } from "../utils/status-code";
+
+export const serviceRegisterPlay = (
+  episode_id: number,
+  user_id: number,
+): AnalyticsTransferModel => {
+  const result = repositoryRegisterPlay(episode_id, user_id);
+
+  return {
+    statusCode: StatusCode.CREATED,
+    body: result,
+  };
+};
+
+export const serviceEpisodeAnalytics = (
+  episode_id: number,
+): AnalyticsTransferModel => {
+  const episodeAnalytics = repositoryEpisodeAnalytics(episode_id);
+
+  return {
+    statusCode: StatusCode.OK,
+    body: episodeAnalytics,
+  };
+};
+
+export const servicePodcastAnalytics = (
+  podcastName: string,
+): AnalyticsTransferModel => {
+  const podcastAnalytics = repositoryPodcastAnalytics(podcastName);
+
+  if (!podcastAnalytics) {
+    return {
+      statusCode: StatusCode.NOT_FOUND,
+      body: [],
+    };
+  }
+
+  return {
+    statusCode: StatusCode.OK,
+    body: [podcastAnalytics],
+  };
+};
